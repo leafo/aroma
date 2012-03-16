ca933342d03dc1d0ed1671f2a72ea4fa90fab07d
leaf corcoran <leafot@gmail.com>
2012-03-16 00:40:15 -0700

    opengles context

--- nacl/aroma.cc
+++ nacl/aroma.cc
@@ -22,7 +22,139 @@ using namespace std;
 
 extern "C" int luaopen_cjson(lua_State *l);
 
+// opengl headers
+#include <GLES2/gl2.h>
+#include "ppapi/c/ppb_opengles2.h"
+#include "ppapi/cpp/graphics_3d_client.h"
+#include "ppapi/cpp/graphics_3d.h"
+#include "ppapi/gles2/gl2ext_ppapi.h"
+
+#include "ppapi/cpp/completion_callback.h"
+#include "ppapi/cpp/rect.h"
+// ~
+
+
 namespace aroma {
+  class OpenGLContext;
+  class AromaInstance;
+  class Renderer;
+
+  void default_flush_callback(void *data, int32_t result) {
+    OpenGLContext* context = (OpenGLContext*)data;
+    // context->render();
+  }
+
+  class OpenGLContext : public pp::Graphics3DClient {
+    protected:
+      Renderer *renderer;
+      const struct PPB_OpenGLES2* gles2_interface;
+      pp::Instance *instance;
+      pp::Graphics3D graphics;
+      pp::Size size;
+
+    public:
+      OpenGLContext(pp::Instance* instance, Renderer* renderer) :
+        renderer(renderer),
+        pp::Graphics3DClient(instance),
+        instance(instance)
+      {
+        pp::Module *module = pp::Module::Get();
+        assert(module);
+        gles2_interface = static_cast<const struct PPB_OpenGLES2*>(
+          module->GetBrowserInterface(PPB_OPENGLES2_INTERFACE));
+        assert(gles2_interface);
+      }
+
+      virtual ~OpenGLContext() {
+        glSetCurrentContextPPAPI(0);
+      }
+
+      void invalidate() {
+        glSetCurrentContextPPAPI(0);
+      }
+
+      bool make_current() {
+        if (graphics.is_null()) {
+          printf("-- initializing graphics3d\n");
+          int32_t attribs[] = {
+            PP_GRAPHICS3DATTRIB_ALPHA_SIZE, 8,
+            PP_GRAPHICS3DATTRIB_DEPTH_SIZE, 24,
+            PP_GRAPHICS3DATTRIB_STENCIL_SIZE, 8,
+            PP_GRAPHICS3DATTRIB_SAMPLES, 0,
+            PP_GRAPHICS3DATTRIB_SAMPLE_BUFFERS, 0,
+            PP_GRAPHICS3DATTRIB_WIDTH, size.width(),
+            PP_GRAPHICS3DATTRIB_HEIGHT, size.height(),
+            PP_GRAPHICS3DATTRIB_NONE
+          };
+          graphics = pp::Graphics3D(instance, pp::Graphics3D(), attribs);
+          if (graphics.is_null()) {
+            glSetCurrentContextPPAPI(0);
+            return false;
+          }
+          instance->BindGraphics(graphics);
+        }
+
+        glSetCurrentContextPPAPI(graphics.pp_resource());
+        return true;
+      }
+
+      void resize(const pp::Size& s) {
+        printf("-- resizing buffers\n");
+        size = s;
+        if (!graphics.is_null()) {
+          graphics.ResizeBuffers(s.width(), s.height());
+        }
+      }
+
+      void flush() {
+        graphics.SwapBuffers(pp::CompletionCallback(&default_flush_callback, this));
+      }
+
+      void Graphics3DContextLost() {
+        assert(!"++ Lost graphics context");
+      }
+
+      int width() {
+        return size.width();
+      }
+
+      int height() {
+        return size.height();
+      }
+  };
+
+
+  class Renderer {
+    protected:
+      OpenGLContext* context;
+      AromaInstance* instance;
+
+    public:
+      Renderer(AromaInstance* instance) : instance(instance) {
+        context = new OpenGLContext((pp::Instance*)instance, this);
+      }
+
+      void draw() {
+        glClear(GL_COLOR_BUFFER_BIT);
+      }
+
+      // called for every frame
+      void tick() {
+        context->make_current();
+
+        glViewport(0, 0, context->width(), context->height());
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+
+        draw();
+        context->flush();
+      }
+
+      void did_change_view(const pp::Rect& pos, const pp::Rect& clip) {
+        context->resize(pos.size());
+        tick();
+      }
+  };
+
 
   void push_var(lua_State* l, pp::Var var) {
     if (var.is_null()) {
@@ -68,6 +200,7 @@ namespace aroma {
   class AromaInstance : public pp::Instance {
     protected:
       lua_State* l;
+      Renderer *renderer;
 
       void bind_function(const char *name, lua_CFunction func) {
         lua_getglobal(l, "nacl");
@@ -89,7 +222,9 @@ namespace aroma {
       }
 
     public:
-      AromaInstance(PP_Instance instance) : pp::Instance(instance) { }
+      AromaInstance(PP_Instance instance) :
+        pp::Instance(instance),
+        renderer(NULL) { }
 
       bool Init(uint32_t argc, const char** argn, const char** argv) {
         l = luaL_newstate();
@@ -134,7 +269,10 @@ namespace aroma {
       }
 
       void DidChangeView(const pp::Rect& pos, const pp::Rect& clip) {
-        PostMessage(pp::Var("DidChangeView"));
+        // PostMessage(pp::Var("DidChangeView"));
+        printf("-- didchangeview\n");
+        if (!renderer) renderer = new Renderer(this);
+        renderer->did_change_view(pos, clip);
       }
 
       lua_State* lua() {
@@ -146,6 +284,10 @@ namespace aroma {
     public:
       AromaModule() : pp::Module() {}
       virtual ~AromaModule() {}
+
+      virtual bool Init() {
+        return glInitializePPAPI(get_browser_interface()) == GL_TRUE;
+      }
 
       virtual pp::Instance* CreateInstance(PP_Instance instance) {
         return new AromaInstance(instance);
