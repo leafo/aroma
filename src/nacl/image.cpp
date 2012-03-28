803515894b91a7af2ec420d48e8b2fcf5270cb7a
leaf corcoran <leafot@gmail.com>
2012-03-27 20:25:52 -0700

    Image.setWrap

--- src/nacl/image.cpp
+++ src/nacl/image.cpp
@@ -3,6 +3,15 @@
 
 namespace aroma {
 
+	GLenum get_wrap_mode(const char* name) {
+		if (strcmp(name, "repeat") == 0) {
+			return GL_REPEAT;
+		} else if (strcmp(name, "clamp") == 0) {
+			return GL_CLAMP_TO_EDGE;
+		}
+		return -1;
+	}
+
 	struct Pixel {
 		byte r, g, b, a;
 
@@ -36,21 +45,46 @@ namespace aroma {
 		glBindTexture(GL_TEXTURE_2D, texid);
 	}
 
-	int Image::_gc(lua_State *l) {
-		Image *self = newuserdata(Image);
+	int Image::_gc(lua_State* l) {
+		Image* self = newuserdata(Image);
 		glDeleteTextures(1, &self->texid);
 		self->texid = -1;
 		return 0;
 	}
 
-	int Image::_getWidth(lua_State *l) {
+	int Image::_getWidth(lua_State* l) {
 		lua_pushnumber(l, getself(Image)->width);
 		return 1;
 	}
 
-	int Image::_getHeight(lua_State *l) {
+	int Image::_getHeight(lua_State* l) {
 		lua_pushnumber(l, getself(Image)->height);
 		return 1;
+	}
+
+	int Image::_setWrap(lua_State* l) {
+		Image* self = getself(Image);
+		int top = lua_gettop(l);
+
+		glBindTexture(GL_TEXTURE_2D, self->texid);
+
+		if (top > 1 && !lua_isnil(l, 2)) {
+			GLenum mode = get_wrap_mode(luaL_checkstring(l, 2));
+			if (mode < 0) {
+				return luaL_error(l, "unknown horizontal wrap mode: %s", luaL_checkstring(l, 2));
+			}
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, mode);
+		}
+
+		if (top > 2 && !lua_isnil(l, 3)) {
+			GLenum mode = get_wrap_mode(luaL_checkstring(l, 3));
+			if (mode < 0) {
+				return luaL_error(l, "unknown vertical wrap mode: %s", luaL_checkstring(l, 3));
+			}
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, mode);
+		}
+
+		return 0;
 	}
 
 	void Image::push(lua_State *l) const {
@@ -60,6 +94,7 @@ namespace aroma {
 			lua_newtable(l); // the index table
 			setfunction("getWidth", _getWidth);
 			setfunction("getHeight", _getHeight);
+			setfunction("setWrap", _setWrap);
 
 			lua_setfield(l, -2, "__index");
 
--- src/nacl/image.h
+++ src/nacl/image.h
@@ -13,10 +13,12 @@ namespace aroma {
 		static Image from_bytes(const byte* bytes, int width, int height, GLenum
 				format = GL_RGBA, GLenum type = GL_UNSIGNED_BYTE);
 
-		static int _gc(lua_State *l);
+		static int _gc(lua_State* l);
 
-		static int _getWidth(lua_State *l);
-		static int _getHeight(lua_State *l);
+		static int _getWidth(lua_State* l);
+		static int _getHeight(lua_State* l);
+
+		static int _setWrap(lua_State* l);
 
 		// only push once, or add refernce counting so gc doesn't remove texture
 		// being used by other images
